e2292ac4acb436bed04e07700f83339015e2b4a9
Martha Hipley <info@marthahipley.com>
2016-11-23 21:12:24 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,68 @@
+This is a chrome extension that replaces the names of Trump & his surrogates with woke call outs. Don't normalize! Stay angry and active!
+
+If you want to do something more useful than make a Chrome Extension, here are some links
+(and please send suggestions for additions to woke@everyoneisugly.com).
+
+##Things to do:
+
+[How White People Can Support People Of Color Now](https://www.buzzfeed.com/hannahgiorgis/how-white-people-can-support-people-of-color-right-now?utm_term=.xfZMkGdxdB#.tuk5y1bObQ)
+
+[A List of Pro-Women, Pro-Immigrant, Pro-Earth, Anti-Bigotry Organizations That Need Your Support](http://jezebel.com/a-list-of-pro-women-pro-immigrant-pro-earth-anti-big-1788752078)
+
+[WHAT TO DO](https://medium.com/@thelindywest/what-to-do-d17e58305fa7#.zh8gwrdt7)
+
+[Companies to Boycott](https://docs.google.com/spreadsheets/d/1vu0Y0HvadMgG_LN7dF8W7M66oPCcx_nmSARQWirV7iY/htmlview?sle=true#gid=0)
+
+[How to Channel Your Post-Election Anger, Sadness, and Fear Into Action](http://www.slate.com/articles/news_and_politics/politics/2016/11/how_liberals_can_channel_their_post_election_anxiety_into_action.html)
+
+[Charities to Donate to in Trump's America](http://www.pajiba.com/seriously_random_lists/charities-to-donate-to-in-trumps-america.php)
+
+[how to contact your congresspeople](https://twitter.com/editoremilye/status/797243415922515970)
+
+[How You Can Help The Standing Rock Sioux Fight The Dakota Access Pipeline](http://www.huffingtonpost.com/entry/help-standing-rock-sioux-dakota-access-pipeline_us_583480c9e4b000af95eca013?)
+
+##Things to read:
+
+[A Time for Treason: a reading list](http://thenewinquiry.com/features/a-time-for-treason/)
+
+[12 Books We're Reading to Understand the Election Result](http://nymag.com/thecut/2016/11/12-books-were-reading-to-understand-the-election-result.html)
+
+[Between the World and Me](https://www.amazon.com/Between-World-Me-Ta-Nehisi-Coates-ebook/dp/B00SEFAIRI/ref=sr_1_1?s=books&ie=UTF8&qid=1479921027&sr=1-1&keywords=between+the+world+and+me)
+
+[How Does it Feel to Be a Problem?](https://www.amazon.com/dp/0143115413/)
+
+[Denying the Holocaust: The Growing Assault on Truth and Memory](https://www.amazon.com/Denying-Holocaust-Growing-Assault-Memory/dp/0452272742)
+
+[Them: Adventures with Extremists](https://www.amazon.com/dp/B00570B692/ref=dp-kindle-redirect?_encoding=UTF8&btkr=1)
+
+[Los Padrinos Juvenile Detention Center, Unit Y2 ](http://hyperallergic.com/338196/one-poem-by-christopher-soto/)
+
+[If You are Over Staying Woke](https://www.poetryfoundation.org/poems-and-poets/poems/detail/58028)
+
+##Things to watch:
+
+[13th](https://www.netflix.com/title/80091741)
+
+##Things to listen to:
+
+[Call Your Girlfriend](http://www.callyourgirlfriend.com/)
+
+[Minority Korner](http://www.maximumfun.org/shows/minority-korner)
+
+[Code Switch](http://www.npr.org/sections/codeswitch/)
+
+##Digital Privacy & Security:
+
+[President Obama Will Soon Turn Over the Keys to the Surveillance State to President-Elect Trump](https://www.aclu.org/blog/speak-freely/president-obama-will-soon-turn-over-keys-surveillance-state-president-elect-trump)
+
+[A Few Easy Steps Everyone Should Take to Protect Their Digital Privacy](https://www.aclu.org/blog/speak-freely/few-easy-steps-everyone-should-take-protect-their-digital-privacy)
+
+[How to Encrypt Your Email and Keep Your Conversations Private](http://lifehacker.com/how-to-encrypt-your-email-and-keep-your-conversations-p-1133495744)
+
+[Install Tor Browser](https://www.torproject.org/projects/torbrowser.html.en)
+
+[Install Signal (iOS)](https://itunes.apple.com/us/app/signal-private-messenger/id874139669?mt=8&ign-mpt=uo%3D4)
+
+[Install Signal (Android](https://play.google.com/store/apps/details?id=org.thoughtcrime.securesms&hl=en)
+
+
---
+++ woke.js
@@ -0,0 +1,273 @@
+
+var elements = document.getElementsByTagName('*');
+
+var dillweeds = require('./woke.json');
+
+function replaceDillweed(dillweed) {
+    var random = Math.floor(Math.random() * dillweeds[dillweed].length);
+    if (dillweeds[dillweed][random].position == 'before') {
+        var dillweedString = '<span><a href="' + dillweeds[dillweed][random].source + '" style="display:inline;" target="_blank">' + dillweeds[dillweed][random].string + '</a></span> ' + dillweeds[dillweed][random].lastname + '<span></span> ';
+    }
+    if (dillweeds[dillweed][random].position == 'after') {
+        var dillweedString = dillweeds[dillweed][random].lastname + '<span></span>, ' + '<span><a href="' + dillweeds[dillweed][random].source + '" style="display:inline;" target="_blank">' + dillweeds[dillweed][random].string + '</a></span>, ';
+    }
+    if (dillweeds[dillweed][random].position == 'replace') {
+        var dillweedString = '<span><a href="' + dillweeds[dillweed][random].source + '" style="display:inline;" target="_blank">' + dillweeds[dillweed][random].string + '</a></span>';
+    }
+    return dillweedString;
+
+};
+
+function handleTextNode(textNode) {
+    if(textNode.nodeName !== '#text') {
+        return;
+    }
+
+    let origText = textNode.textContent;
+
+    let newHtml=origText
+
+    //HERE IS THE LOGIC FOR REPLACEMENT
+        .replace(/Donald Trump /gi, replaceDillweed("Trump"))
+        .replace(/Donald Trump, /gi, replaceDillweed("Trump"))
+      
+        .replace(/Mike Pence /gi, replaceDillweed("Pence"))
+        .replace(/Mike Pence, /gi, replaceDillweed("Pence"))
+
+        .replace(/Steve Bannon /gi, replaceDillweed("Bannon"))
+        .replace(/Steve Bannon, /gi, replaceDillweed("Bannon"))
+        .replace(/Stephen Bannon /gi, replaceDillweed("Bannon"))
+        .replace(/Stephen Bannon, /gi, replaceDillweed("Bannon"))     
+
+        .replace(/Jared Kushner /gi, replaceDillweed("Kushner"))
+        .replace(/Jared Kushner, /gi, replaceDillweed("Kushner"))
+
+        .replace(/Rudy Giuliani /gi, replaceDillweed("Giuliani"))
+        .replace(/Rudy Giuliani, /gi, replaceDillweed("Giuliani"))
+
+        .replace(/Newt Gingrich /gi, replaceDillweed("Gingrich"))
+        .replace(/Newt Gingrich, /gi, replaceDillweed("Gingrich"))
+
+        .replace(/Reince Priebus /gi, replaceDillweed("Priebus"))
+        .replace(/Reince Priebus, /gi, replaceDillweed("Priebus"))
+
+
+        .replace(/Jeff Sessions /gi, replaceDillweed("Sessions"))
+        .replace(/Jeff Sessions, /gi, replaceDillweed("Sessions")) 
+
+        .replace(/Kellyanne Conway /gi, replaceDillweed("Conway"))
+        .replace(/Kellyanne Conway, /gi, replaceDillweed("Conway"))
+
+        .replace(/Chris Christie /gi, replaceDillweed("Christie"))
+        .replace(/Chris Christie, /gi, replaceDillweed("Christie"))
+
+        .replace(/Sarah Palin /gi, replaceDillweed("Palin"))
+        .replace(/Sarah Palin, /gi, replaceDillweed("Palin"))
+
+        .replace(/John R\. Bolton /gi, replaceDillweed("Bolton"))
+        .replace(/John R\. Bolton, /gi, replaceDillweed("Bolton"))
+        .replace(/John Bolton /gi, replaceDillweed("Bolton"))
+        .replace(/John Bolton, /gi, replaceDillweed("Bolton")) 
+
+        .replace(/Jeb Hensarling /gi, replaceDillweed("Hensarling"))
+        .replace(/Jeb Hensarling, /gi, replaceDillweed("Hensarling"))
+
+        .replace(/Steven Mnunchin /gi, replaceDillweed("Mnunchin"))
+        .replace(/Steven Mnunchin, /gi, replaceDillweed("Mnunchin"))
+
+        .replace(/Jan Brewer /gi, replaceDillweed("Brewer"))
+        .replace(/Jan Brewer, /gi, replaceDillweed("Brewer"))
+
+        .replace(/Ben Carson /gi, replaceDillweed("Carson"))
+        .replace(/Ben Carson, /gi, replaceDillweed("Carson"))
+
+        .replace(/Mike Huckabee /gi, replaceDillweed("Huckabee"))
+        .replace(/Mike Huckabee, /gi, replaceDillweed("Huckabee"))  
+
+        .replace(/Bobby Jindal /gi, replaceDillweed("Jindal"))
+        .replace(/Bobby Jindal, /gi, replaceDillweed("Jindal"))   
+
+        .replace(/Rick Scott /gi, replaceDillweed("Scott"))
+        .replace(/Rick Scott, /gi, replaceDillweed("Scott"))
+
+        .replace(/Joe Arpaio /gi, replaceDillweed("Arpaio"))
+        .replace(/Joe Arpaio, /gi, replaceDillweed("Arpaio"))     
+
+        .replace(/David A\. Clark /gi, replaceDillweed("Clark"))
+        .replace(/David A\. Clark, /gi, replaceDillweed("Clark")) 
+        .replace(/David Clark /gi, replaceDillweed("Clark"))
+        .replace(/David Clark, /gi, replaceDillweed("Clark")) 
+
+        .replace(/Paul Ryan /gi, replaceDillweed("Ryan"))
+        .replace(/Paul Ryan, /gi, replaceDillweed("Ryan"))
+
+        .replace(/Myron Ebell /gi, replaceDillweed("Ebell"))
+        .replace(/Myron Ebell, /gi, replaceDillweed("Ebell"))    
+
+        .replace(/Michelle Rhee /gi, replaceDillweed("Rhee"))
+        .replace(/Michelle Rhee, /gi, replaceDillweed("Rhee"))  
+
+        .replace(/Frank Gaffney /gi, replaceDillweed("Gaffney"))
+        .replace(/Frank Gaffney, /gi, replaceDillweed("Gaffney"))     
+
+        .replace(/Michael Flynn /gi, replaceDillweed("Flynn"))
+        .replace(/Michael Flynn, /gi, replaceDillweed("Flynn"))    
+
+        .replace(/Peter Thiel /gi, replaceDillweed("Thiel"))
+        .replace(/Peter Thiel, /gi, replaceDillweed("Thiel"))    
+
+        .replace(/Milo Yiannopoulos /gi, replaceDillweed("Yiannopoulos"))
+        .replace(/Milo Yiannopoulos, /gi, replaceDillweed("Yiannopoulos"))                 
+
+        .replace(/Mike Pompeo /gi, replaceDillweed("Pompeo"))
+        .replace(/Mike Pompeo, /gi, replaceDillweed("Pompeo")) 
+
+        .replace(/Michael McCaul /gi, replaceDillweed("McCaul"))
+        .replace(/Michael McCaul, /gi, replaceDillweed("McCaul"))     
+
+        .replace(/Mike Rogers /gi, replaceDillweed("Rogers"))
+        .replace(/Mike Rogers, /gi, replaceDillweed("Rogers"))   
+
+        .replace(/Kris Kobach /gi, replaceDillweed("Kobach"))
+        .replace(/Kris Kobach, /gi, replaceDillweed("Kobach"))   
+
+        .replace(/alt-right/gi, replaceDillweed("altright"))
+        .replace(/Alt-right/gi, replaceDillweed("altright"))   
+
+        .replace(/white nationalist/gi, replaceDillweed("nationalist"))
+        .replace(/white nationalists/gi, replaceDillweed("nationalists")) 
+
+        .replace(/clean coal/gi, replaceDillweed("coal"))
+        
+        .replace(/anti-vaxxers/gi, replaceDillweed("vax"))     
+
+        .replace(/fake news/gi, replaceDillweed("news"))
+
+        .replace(/make America great again/gi, replaceDillweed("america"))
+
+        .replace(/Tom Price /gi, replaceDillweed("Price"))
+        .replace(/Tom Price, /gi, replaceDillweed("Price"));                                              
+
+        // LOGIC FOR LAST NAMES ONLY
+
+      let finalHtml=newHtml  
+        .replace(/Trump /gi, replaceDillweed("Trump"))
+        .replace(/Trump, /gi, replaceDillweed("Trump"))
+
+        .replace(/Priebus /gi, replaceDillweed("Priebus"))
+        .replace(/Priebus, /gi, replaceDillweed("Priebus"))
+      
+        .replace(/Pence /gi, replaceDillweed("Pence"))
+        .replace(/Pence, /gi, replaceDillweed("Pence"))
+
+        .replace(/Bannon /gi, replaceDillweed("Bannon"))
+        .replace(/Bannon, /gi, replaceDillweed("Bannon"))
+
+        .replace(/Kushner /gi, replaceDillweed("Kushner"))
+        .replace(/Kushner, /gi, replaceDillweed("Kushner"))
+
+        .replace(/Giuliani /gi, replaceDillweed("Giuliani"))
+        .replace(/Giuliani, /gi, replaceDillweed("Giuliani"))
+
+        .replace(/Gingrich /gi, replaceDillweed("Gingrich"))
+        .replace(/Gingrich, /gi, replaceDillweed("Gingrich"))
+
+        .replace(/Sessions /gi, replaceDillweed("Sessions"))
+        .replace(/Sessions, /gi, replaceDillweed("Sessions")) 
+
+        .replace(/Conway /gi, replaceDillweed("Conway"))
+        .replace(/Conway, /gi, replaceDillweed("Conway"))
+
+        .replace(/Christie /gi, replaceDillweed("Christie"))
+        .replace(/Christie, /gi, replaceDillweed("Christie"))
+
+        .replace(/Palin /gi, replaceDillweed("Palin"))
+        .replace(/Palin, /gi, replaceDillweed("Palin"))
+
+        .replace(/Hensarling /gi, replaceDillweed("Hensarling"))
+        .replace(/Hensarling, /gi, replaceDillweed("Hensarling"))
+
+        .replace(/Mnunchin /gi, replaceDillweed("Mnunchin"))
+        .replace(/Mnunchin, /gi, replaceDillweed("Mnunchin"))
+
+        .replace(/Huckabee /gi, replaceDillweed("Huckabee"))
+        .replace(/Huckabee, /gi, replaceDillweed("Huckabee"))  
+
+        .replace(/Jindal /gi, replaceDillweed("Jindal"))
+        .replace(/Jindal, /gi, replaceDillweed("Jindal"))   
+
+        .replace(/Arpaio /gi, replaceDillweed("Arpaio"))
+        .replace(/Arpaio, /gi, replaceDillweed("Arpaio"))     
+
+        .replace(/Ebell /gi, replaceDillweed("Ebell"))
+        .replace(/Ebell, /gi, replaceDillweed("Ebell"))    
+
+        .replace(/Rhee /gi, replaceDillweed("Rhee"))
+        .replace(/Rhee, /gi, replaceDillweed("Rhee"))  
+
+        .replace(/Gaffney /gi, replaceDillweed("Gaffney"))
+        .replace(/Gaffney, /gi, replaceDillweed("Gaffney"))        
+
+        .replace(/Thiel /gi, replaceDillweed("Thiel"))
+        .replace(/Thiel, /gi, replaceDillweed("Thiel"))    
+
+        .replace(/Yiannopoulos /gi, replaceDillweed("Yiannopoulos"))
+        .replace(/Yiannopoulos, /gi, replaceDillweed("Yiannopoulos"))                 
+
+        .replace(/Pompeo /gi, replaceDillweed("Pompeo"))
+        .replace(/Pompeo, /gi, replaceDillweed("Pompeo"))  
+
+        .replace(/Kobach /gi, replaceDillweed("Kobach"))
+        .replace(/Kobach, /gi, replaceDillweed("Kobach"));  
+
+    // let finalHtml= newHtml
+
+    if( finalHtml !== origText) {
+        let newSpan = document.createElement('span');
+        newSpan.innerHTML = finalHtml;
+        textNode.parentNode.replaceChild(newSpan,textNode);
+    }
+}
+
+function processDocument() {
+    let treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT,{
+        acceptNode: function(node) { 
+            if(node.textContent.length === 0) {
+                return NodeFilter.FILTER_SKIP; //Skip empty text nodes
+            } //else
+            return NodeFilter.FILTER_ACCEPT;
+        }
+    }, false );
+    while(treeWalker.nextNode()) {
+        handleTextNode(treeWalker.currentNode);
+    }
+}
+
+//is this doing anything??
+// var insertedNodes = [];
+// var observer = new WebKitMutationObserver(function(mutations) {
+//     mutations.forEach(function(mutation) {
+//         for(var i = 0; i < mutation.addedNodes.length; i++)
+//             insertedNodes.push(mutation.addedNodes[i]);
+//     })
+//     console.log(insertedNodes);
+// });
+// observer.observe(document, {
+//     childList: true
+// });
+// console.log(insertedNodes);
+
+window.onload = function() {
+    processDocument();
+    insertionQ('.stream-item').every(function(element){
+
+        processDocument();
+      });    
+
+    // insertionQ('div').every(function(element){
+    //     processDocument();
+    //     console.log("updating");
+    // });
+  };
+
+$(window).scroll(function () { processDocument(); });
